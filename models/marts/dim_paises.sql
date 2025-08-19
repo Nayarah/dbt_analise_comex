81e219975dfa3d53c688da3388c538ed8c9bbda5
nayara <nayara_franco_almeida@hotmail.com>
2025-08-19 10:54:54 -0300

    feat(marts): Cria modelo de dimensão para paises

---
+++ models/marts/dim_paises.sql
@@ -0,0 +1,22 @@
+-- models/marts/dim_paises.sql
+
+-- Cria uma tabela dimensão para a tradução do país
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+WITH source_dicionario AS (
+    -- Seleciona a tabela dicionário que contém todas as traduções
+    SELECT * FROM {{ source('br_me_comex_stat', 'dicionario') }}
+)
+
+SELECT
+    CAST(chave AS STRING) AS id_pais,
+    CAST(valor AS STRING) AS nome_pais
+FROM
+    source_dicionario
+WHERE
+    -- Filtra para pegar apenas as linhas que correspondem ao dicionário de países
+    id_tabela = 'pais'
